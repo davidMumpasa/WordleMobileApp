1fb7edb332838f6151b9ecf08b067bb3bc0d5ec3
hp <davidebula642@gmail.com>
2022-05-10 00:33:47 +0200

    done with the design

--- app/src/main/java/com/example/wordle/MainActivity.java
+++ app/src/main/java/com/example/wordle/MainActivity.java
@@ -1,8 +1,18 @@
 package com.example.wordle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
 }
